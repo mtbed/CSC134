5625f10085cbc4ef9af16bc070bbf047204e0d95
mtbed <bledsolm9773@student.faytechcc.edu>
2023-08-28 11:30:22 -0400

    M1LAB taken from Repl.it
    I coded this earlier in Repl.it and copied it over

---
+++ M1LAB/M1LAB/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   using namespace std;
+  // VARIABLES
+  /*CSC 134
+  M1LAB
+  Matthew Bedsole
+  8/28/23
+  */
+  string orchard_name = "Matt";
+  int apples_stocked = 100;
+  float apples_price = 0.25;
+  int amt_bought = 0;
+  float payment;
+
+
+  cout << "\nHello Welcome to " << orchard_name << "'s apple orchard." << endl;
+
+  cout << "We have " << apples_stocked << " apples in stock" << endl;
+
+  cout << "Apples are currently $" << apples_price << " each." << endl;
+
+  // ASK FOR USER AMOUNT
+
+  while (amt_bought < 1){
+  cout << "\nHow many apples do you want?" << endl;
+  cin >> amt_bought;
+
+
+  if (amt_bought > apples_stocked){
+    cout << "YOU BOUGHT MORE THAN WE GOT" << endl;
+  }
+  if (amt_bought < 0){
+    cout << "YOU CANNOT BUY LESS THAN 1" << endl;
+  }
+  else{
+    payment = apples_price * amt_bought;
+    cout << "\nYou bought \n" << amt_bought << " apples\nat " << apples_price << " per apple." << "\n\nYour total is: $" << payment << endl;
+  }
+  }
+}
