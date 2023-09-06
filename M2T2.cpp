ff9725850aac04b248505347f552cae6a65765a4
mtbed <bledsolm9773@student.faytechcc.edu>
2023-09-06 11:35:43 -0400

    unfinished
    Copied from replit as base, need to update some more I think

---
+++ M2T2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+  // VARIABLES
+  string orchard_name = "Matt";
+  int apples_stocked = 100;
+  float apples_price = 0.25;
+  int amt_bought = 0;
+  float payment;
+  string customer_name;
+
+
+  cout << "\nHello Welcome to " << orchard_name << "'s apple orchard.\n" << endl;
+
+  cout << "What's your name?" << endl;
+  getline (cin, customer_name);
+  cout << "Welcome, " << customer_name << "\n" << endl;
+
+  cout << "We have " << apples_stocked << " apples in stock" << endl;
+
+  cout << "Apples are currently $" << apples_price << " each." << endl;
+
+  // ASK FOR USER AMOUNT
+
+  while (amt_bought < 1){
+  cout << "\nHow many apples do you want?" << endl;
+  cin >> amt_bought;
+
+
+  if (amt_bought > apples_stocked){
+    cout << "YOU BOUGHT MORE THAN WE GOT" << endl;
+  }
+  if (amt_bought < 0){
+    cout << "YOU CANNOT BUY LESS THAN 1" << endl;
+  }
+  else{
+    payment = apples_price * amt_bought;
+    cout << "\nYou bought \n" << amt_bought << " apples\nat " << apples_price << " per apple." << "\n\nYour total is: $" << payment << endl;
+  }
+  }
+}
