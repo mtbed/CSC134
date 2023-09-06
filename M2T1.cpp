84cd1bdf0bc2a587b060c3ae03becb144b9baf7f
mtbed <bledsolm9773@student.faytechcc.edu>
2023-09-06 11:07:08 -0400

    edits

--- M2T1.cpp
+++ M2T1.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
+// M2T1
+// Receipt Calculator
+// Matthew Bedsole
+// 9/6/23
+
+
 
 int main() {
-  using namespace std;
+    using namespace std;
 
 
 
-  double mealPrice = 5.99;
-  double taxRate = 0.08;
-  double taxAmount;
-  double total;
+    double mealPrice = 5.99;
+    double taxRate = 0.08;
+    double taxAmount;
+    double total;
 
 
-  taxAmount = taxRate * mealPrice;
+taxAmount = taxRate * mealPrice;
+total = mealPrice + taxAmount;
 
 
   cout << "Thank you for dining with us." << endl;
   cout << "Your meal total is $" << mealPrice << endl;
   cout << "The tax is $" << taxAmount << endl;
+  cout << "Your total is $" << total << endl;
+  cout << "Please come again." << endl;
 
 
 }
