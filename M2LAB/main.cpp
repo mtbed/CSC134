e77b75f846d873d5725820052567c09bdb00f437
mtbed <bledsolm9773@student.faytechcc.edu>
2023-09-11 11:09:03 -0400

    M2LAB draft (final?)

--- M2LAB/main.cpp
+++ M2LAB/main.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+double length;
+double width;
+double height;
+double volume;
+
+const double COST_PER_CUBIC_FOOT = 0.23;
+const double PRICE_PER_CUBIC_FOOT = 0.5;
+
+double cost;
+double price;
+double profit;
+
+    cout << "Hello, this program will calculate the following:\n" << endl;
+    cout << "-Volume (in feet) \n-Cost \n-Customer price \n-Profit of any crate GCI builds \n\n" << endl;
+
+
+    cout << "Enter the dimensions of your crate (in feet):\n";
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
+
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    price = volume * PRICE_PER_CUBIC_FOOT;
+    profit = price - cost;
+
+    cout << "\n\n ----------------------------- \n\n";
+    cout << "CALCULATIONS: \n\n";
+
+
+    cout << "Volume: " << volume << "ft" << endl;
+    cout << "Cost: $" << cost << endl;
+    cout << "Price: $" << price << endl;
+    cout << "Profit: $" << profit << endl;
+
+
+
+
+
     return 0;
 }
