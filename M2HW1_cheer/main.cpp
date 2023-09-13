9da1ecf859aeb7d314f2d224878e9cdbff52813e
mtbed <bledsolm9773@student.faytechcc.edu>
2023-09-13 11:39:13 -0400

    done

--- M2HW1_cheer/main.cpp
+++ M2HW1_cheer/main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string letsGo = "Let's go";
+    string school = " FTCC";
+    string team = " Trojans";
+    string cheerOne;
+    string cheerTwo;
+
+
+    cout << "THIS IS CHEER BOT.\n\nI WILL CHEER NOW.\n\n" << endl;
+
+
+
+    cout << letsGo << school << endl;
+    cout << letsGo << team << endl;
+    cout << letsGo << school << endl;
+    cout << letsGo << team << endl;
+
+
+
     return 0;
 }
