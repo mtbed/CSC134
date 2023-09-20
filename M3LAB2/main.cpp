0362f23821c71b095bb97e2b46ddafa5243c7393
mtbed <bledsolm9773@student.faytechcc.edu>
2023-09-20 11:30:10 -0400

    in_battle

--- M3LAB2/main.cpp
+++ M3LAB2/main.cpp
@@ -34,6 +34,8 @@ int main() {
   int pokeroll = roll();
   string pokemon;
   int chance;
+  bool in_battle = true;
+
 
 if (pokeroll == 1){
     pokemon = "bidoof";
@@ -50,6 +52,10 @@ else if (pokeroll == 4){
 
   // ask the question
   cout << "You've encountered a wild Pokemon!" << endl;
+
+
+
+  while in_battle{
   cout << "It's a " << pokemon << "!\n\n";
 
   cout << "OPTIONS:\n1. Attack     2.Catch     3.Run\n\n";
@@ -63,14 +69,39 @@ else if (pokeroll == 4){
   	if (pokemon != "mewtwo" ){
         chance = roll();
         if (chance != 4){
-            cout << "Nice! You won!\n\n";
+            cout << "Nice! You won!\n";
+            in_battle = false;
+            if (pokemon != "charizard"){
+                cout << "+ 43 EXP\n";
+                in_battle = false;
+            }
+            else{
+                cout << "+ 2143 EXP\n";
+                in_battle = false;
+            }
         }
         else if (chance == 4){
             cout << "Oh no! you've missed and lost the battle!\n\n";
+            in_battle = false;
         }
   	}
     if (pokemon == "mewtwo"){
-        cout << "YOU LOST\n\n";
+        chance == roll();
+        if (chance == 4){
+            if (chance == 4){
+                cout << "YOU WON!\n +16421 EXP";
+                in_battle = false;
+            }
+            else{
+                cout << "YOU LOST\n\n";
+                in_battle = false;
+            }
+        }
+        else{
+            cout << "YOU LOST\n\n";
+            in_battle = false;
+        }
+
     }
   }
   else if (2 == choice) {
@@ -80,22 +111,27 @@ else if (pokeroll == 4){
             chance = roll();
             if (chance ==4){
                 cout << "Oh no! They got away!\n\n";
+                in_battle = false;
             }
             else{
             cout << "You caught a " << pokemon << "!!\n\n";
+            in_battle = false;
         }
         }
         else{
             cout << "You caught a " << pokemon << "!!\n\n";
+            in_battle = false;
         }
   	}
   	else if (pokemon == "charizard"){
         chance = roll();
         if (chance >= 3){
             cout << "You caught a " << pokemon << "!!\n\n";
+            in_battle = false;
         }
         else{
             cout << "Oh no! They got away!\n\n";
+            in_battle = false;
         }
   	}
   	else if (pokemon == "mewtwo"){
@@ -104,19 +140,52 @@ else if (pokeroll == 4){
             chance = roll();
             if (chance == 4){
                 cout << "No way! You caught a " << pokemon << "!!\n\n";
+                in_battle = false;
             }
             else{
-                cout << "Agh! Almost had it!!";
+                cout << "Agh! Almost had it!!\n";
+                in_battle = false;
             }
         }
         else{
             cout << "Oh no! They got away!\n\n";
+            in_battle = false;
         }
   	}
 
 
   }
 
+  else if (choice == 3){
+    if (pokemon == "bidoof" || pokemon == "squirtle"){
+        cout << "Got away safely!\n";
+        in_battle = false;
+    }
+    else if (pokemon == "charizard"){
+        chance == roll();
+        if (chance == 4){
+            cout << "Oh no! Can't escape!\n\n";
+            in_battle = true;
+        }
+        else{
+            cout << "Got away safely!\n";
+            in_battle = false;
+        }
+    }
+    else if (pokemon == "mewtwo"){
+        chance == roll();
+        if (chance == 4){
+            cout << "Got away safely!\n";
+            in_battle = false;
+        }
+        else{
+            cout << "Oh no! Can't escape!\n\n";
+            in_battle = true;
+        }
+    }
+
+  }
+}
 
 
 
@@ -128,7 +197,7 @@ else if (pokeroll == 4){
   }
 
   // finish up
-  cout << "Thanks for playing!" << endl; // this runs no matter what they choose
+  cout << "\nThanks for playing!" << endl; // this runs no matter what they choose
   return 0; // exit without error
 
 } // end of the main() method
