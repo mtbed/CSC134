1cecdef9e07baf27516af036631d35f00a9bf601
mtbed <bledsolm9773@student.faytechcc.edu>
2023-09-27 11:16:12 -0400

    broken rn

--- Grade calculator/main.cpp
+++ Grade calculator/main.cpp
@@ -14,30 +14,54 @@ int main()
     cout << "Enter your number grade: ";
     cin >> grade_num;
 
+    for (int i = 0; i <= 4; i++){
+        if (grade_num >= 100 - i * POINT_SCALE){
 
+            if (i <= 1){
+             letter_grade = "A";
+            }
+
+            else if (i == 2){
+             letter_grade = "B";
+            }
+            else if (i == 3){
+             letter_grade = "C";
+            }
+            else if (i == 4){
+             letter_grade = "D";
+            }
+        }
+        else{
+            letter_grade = "F";
+        }
+    }
+
+/*
     if (grade_num >= 100 - POINT_SCALE){
         letter_grade = "A";
     }
 
-    else if (grade_num >= 100 - 2 * POINT_SCALE){
+    else if (grade_num >= 100 - POINT_SCALE * 2){
         letter_grade = "B";
     }
 
-    else if (grade_num >= 100 - 3 * POINT_SCALE){
+    else if (grade_num >= 100 - POINT_SCALE * 3){
         letter_grade = "C";
     }
 
-    else if (grade_num >= 100 - 4 * POINT_SCALE){
+    else if (grade_num >= 100 - POINT_SCALE * 4){
         letter_grade = "D";
     }
 
-    else if (grade_num < 100 - 4 * POINT_SCALE){
+    else if (grade_num < 100 - POINT_SCALE * 4){
         letter_grade = "F";
     }
 
     else{
         cout << "INVALID GRADE\n\n";
     }
+
+    */
 
 
     cout << "A grade of " << grade_num << " is " << letter_grade;
